f183ec4a1c22f41006e6f498a9bc5e5795cf0186
Pedro Lucas Oliveira de Sousa <20231011110035@ifrn.local>
2024-03-01 17:55:43 -0300

    aula04

---
+++ Aula_2/aula03/ex02.py
@@ -0,0 +1,3 @@
+n = str(input("digite seu nome: "))
+a = n.split()
+print("Bem-Vindo ao Python,",a[0])
---
+++ Aula_2/aula03/ex03.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite a nota do primeiro bimestre da disciplina: '))
+n2 = int(input('Digite a nota do segundo bimestre da disciplina: '))
+
+m = (n1*2) + (n2*3) / 2
+
+print('média parcial =')
