c7c1de8a993d40308161684764e81c5607f5878a
Pedro Lucas Oliveira de Sousa <20231011110035@ifrn.local>
2024-03-15 17:44:18 -0300

    a

--- ListaRevisão01/ex03.py
+++ ListaRevisão01/ex03.py
@@ -3,3 +3,31 @@ b = int(input())
 c = int(input())
 d = int(input())
 
+print(a,b,c,d)
+par = []
+impar = []
+
+if a%2==0:
+    par.append(a)
+if b%2==0:
+    par.append(b)
+if c%2==0:
+    par.append(c)
+if d%2==0:
+    par.append(d)
+
+soma1 = sum(par)
+
+if a%2!=0:
+    impar.append(a)
+if b%2!=0:
+    impar.append(b)
+if c%2!=0:
+    impar.append(c)
+if d%2!=0:
+    impar.append(d)
+
+soma2 = sum(impar)
+
+print(f"soma dos pares {soma1}")
+print(f"soma dos impares {soma2}")
---
+++ ex04.py
@@ -0,0 +1,4 @@
+a = str(input("Digite o horario no formato hh:mm "))
+print(a)
+b = str(input("Digite o horario no formato hh:mm "))
+print(b)
