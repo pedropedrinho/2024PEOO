b5a850e1c03d64a278722b67490ecc5343a7e051
pedro lucas <161077784+pedropedrinho@users.noreply.github.com>
2024-08-25 16:51:23 +0000

    com validação de email

--- Projeto/cliente.py
+++ Projeto/cliente.py
@@ -4,29 +4,27 @@
 # U - Update - atualiza os dados de um cliente
 # D - Delete - remove um cliente do cadastro
 import json
+import re
 
 # Modelo
 class Cliente:
   def __init__(self, id, nome, email, fone):
-    self.__id = id
-    self.__nome = nome
-    self.__email = email
-    self.__fone = fone
-
-    def set_nome(self, n):
-        if type(self.__nome) == type(""):
-          self.__nome = n
-    def get_nome(self):
-      return self.__
-
-        
-
+    self.id = id
+    self.nome = nome
+    self.email = email
+    self.fone = fone
+  
   def __str__(self):
-    return f"{self.__id} - {self.__nome} - {self.__email} - {self.__fone}"
+    return f"{self.id} - {self.nome} - {self.email} - {self.fone}"
+  
+  def validar_email(email):
+    # Define um padrão de expressão regular para validação de e-mail
+    padrao_email = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+    return padrao_email.match(email) is not None
 
 # Persistência
 class Clientes:
-  objetos = []  # atributo estático
+  objetos = []  # atributo estáticof
   @classmethod
   def inserir(cls, obj):
     cls.abrir()
@@ -80,13 +78,13 @@ class Clientes:
 class UI:
   @staticmethod
   def menu():
-    print("1 - Inserir cliente, 2 - listar clientes, 3 - atualizar cliente, 4 - excluir cliente, 9 - fim")
+    print("1 - Inserir cliente, 2 - listar clientes, 3 - atualizar cliente, 4 - excluir cliente, 5 - fim")
     return int(input("Informe uma opção: "))
 
   @staticmethod
   def main():
     op = 0
-    while op != 9: 
+    while op != 5: 
       op = UI.menu()
       if op == 1: UI.cliente_inserir()
       if op == 2: UI.cliente_listar()
@@ -98,6 +96,9 @@ class UI:
     #id = int(input("Informe o id: "))
     nome = input("Informe o nome: ")
     email = input("Informe o e-mail: ")
+    if not Cliente.validar_email(email):
+        print("E-mail inválido.")
+        return
     fone = input("Informe o fone: ")
     c = Cliente(0, nome, email, fone)
     Clientes.inserir(c)
@@ -113,6 +114,9 @@ class UI:
     id = int(input("Informe o id do cliente a ser atualizado: "))
     nome = input("Informe o novo nome: ")
     email = input("Informe o novo e-mail: ")
+    if not Cliente.validar_email(email):
+        print("E-mail inválido.")
+        return
     fone = input("Informe o novo fone: ")
     c = Cliente(id, nome, email, fone)
     Clientes.atualizar(c)
@@ -124,4 +128,4 @@ class UI:
     c = Cliente(id, "", "", "")
     Clientes.excluir(c)
 
-UI.main() 
+UI.main()    
